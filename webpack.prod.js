c31e438cbeff4f7ef047fcf0deb880b0ba53f263
Ivan <iv.xromov@mail.ru>
2024-02-02 19:12:46 +0300

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,10 +4,10 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
-  output: {
-    // Tweak this to match your GitHub project name
-    publicPath: "/pixi-tanks/",
-  },
+  // output: {
+  //   // Tweak this to match your GitHub project name
+  //   publicPath: "/",
+  // },
   plugins: [
     new CopyPlugin({
       patterns: [{ from: "assets", to: "" }],
